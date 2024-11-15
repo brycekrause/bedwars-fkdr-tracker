117b4b9bbb73a5a365d0ffa3bd1a151b2c7a2ce2
imjustjayk <brycekrause@icloud.com>
2024-11-15 09:02:48 -0600

    script development. fetch requests implemented

--- script.js
+++ script.js
@@ -10,25 +10,33 @@ let api_key = '6c8d67e6-45b4-4665-bcbc-35dc0ff54d3e';
 //let hypixelurl = `https://api.hypixel.net/player?key=${api_key}&uuid=`;
 
 async function getStats(){
-    user = 'luvonox';
-    let mojangurl = `https://api.mojang.com/users/profiles/minecraft/${user}`;
-    const mojangResponse = await fetch(mojangurl);
-    if (!mojangResponse.ok){
-        throw new Error("Failed to fetch from Mojang API");
+    let user = document.getElementById("player_name").value;
+    let mojangurl = `https://api.ashcon.app/mojang/v2/user/${user}`;
+    try{
+        const mojangResponse = await fetch(mojangurl);
+
+        if (!mojangResponse.ok){
+            throw new Error("Failed to fetch from Mojang API");
+        }
+        const mojangData = await mojangResponse.json();
+        let uuid = mojangData.uuid;
+
+        let hypixelurl = `https://api.hypixel.net/player?key=${api_key}&uuid=${uuid}`;
+        const hypixelResponse = await fetch(hypixelurl);
+        if (!hypixelResponse.ok){
+            throw new Error("Failed to fetch from Hypixel API");
+        }
+        const hypixelData = await hypixelResponse.json();
+
+        let ign = hypixelData.player.displayname;
+        let star = hypixelData.player.achievements.bedwars_level;
+        let fkills = hypixelData.player.stats.Bedwars.final_kills_bedwars;
+        let fdeaths = hypixelData.player.stats.Bedwars.final_deaths_bedwars;
+        let fkdr = (fkills/fdeaths).toFixed(2);
+        let nextFKDR = Math.floor(fkdr) + 1;
+        FinalsTo_nextFKDR = fdeaths * nextFKDR - fkills;
+        document.writeln(`${star} ${ign} has ${fkdr} FKDR and needs ${FinalsTo_nextFKDR} to ${nextFKDR}`);
+    }catch (error){
+        document.writeln(error);
     }
-    const mojangData = await mojangResponse.json();
-
-    let ign = mojangData.name;
-    let uuid = mojangData.id;
-
-    let hypixelurl = `https://api.hypixel.net/player?key=${api_key}&uuid=${uuid}`;
-    const hypixelResponse = await fetch(hypixelurl);
-    if (!hypixelResponse.ok){
-        throw new Error("Failed to fetch from Hypixel API");
-    }
-    const hypixelData = await hypixelResponse.json();
-
-    console.log(hypixelData);
 }
-
-getStats();
